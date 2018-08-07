7564ad1472d7966c48e9457f312ebb94e0375653
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-07 12:27:17 +0100

    navigation branding and logo to SVG

--- resources/views/partials/core/header.blade.php
+++ resources/views/partials/core/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <img src="/images/logo-white.png"  class="logo" alt="">
+                <img src="/images/spectrum_logo.svg" height="100px"  class="logo" alt="">
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <div class="advert">
