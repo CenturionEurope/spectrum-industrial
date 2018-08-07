23c7b60cd063ac48eada5463db5d5010b796f815
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-07 17:00:30 +0100

    voyager constants

--- resources/views/partials/core/head.blade.php
+++ resources/views/partials/core/head.blade.php
@@ -6,14 +6,14 @@
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <meta name='keywords' content='your, tags'/>
     <meta name='description' content='150 Characters'/>
-    <meta name='copyright' content='company name'>
+    <meta name='copyright' content='{{setting('site.title')}}'>
     <meta name='author' content='Centurion'>
     <!-- Open Graph Tags -->
-    <meta name='og:title' content='Website Title'/>
+    <meta name='og:title' content='{{setting('site.title')}}'/>
     <meta name='og:type' content='website'/>
     <meta name='og:url' content=''/>
     <meta name='og:image' content='/image/path'/>
-    <meta name='og:site_name' content='Website Title'/>
+    <meta name='og:site_name' content='{{setting('site.title')}}'/>
     <meta name='og:description' content='Copy Meta Description'/>
     <!-- Favicons -->
     <link rel='icon' type='image/x-icon' href='/images/spectrum.ico' />
@@ -29,6 +29,6 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     
-    <!-- Fontawesome -->
+    <!-- Fontawesome CSS -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 </head>
