db29f5cc3ba99d70bb38a352b5b85e366beaf7ad
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-07 09:59:11 +0100

    logo & admin splash

--- resources/views/partials/core/head.blade.php
+++ resources/views/partials/core/head.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Spectrum Industrial</title>
+    <title>{{setting('site.title')}}</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <meta name='keywords' content='your, tags'/>
