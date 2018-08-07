23d5058e2cdb2affdc53b33d270d10b4b461fb5d
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-07 16:31:59 +0100

    voyager footer

--- resources/views/partials/core/footer.blade.php
+++ resources/views/partials/core/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">        
         <div class="row">
             <div class="col-lg-3">
-            <h5>Find Us</h5>
+            <h5>Find Us</h5>           
                 <hr>
             </div>
             <div class="col-lg-3">
@@ -21,7 +21,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="copyright">
-                    <p>Copyright {{date('Y')}} Spectrum Industrial. All Rights Reserved</p>
+                    <p>Copyright {{date('Y')}} {{setting('company.company_name')}}. All Rights Reserved</p>
                 </div>            
             </div>
         </div>
