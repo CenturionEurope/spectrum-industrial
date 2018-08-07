598e3a4d6b861693291e880473f714066ce1831c
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-07 15:42:38 +0100

    image compression, navigation tweaks

--- resources/views/partials/core/main-nav.blade.php
+++ resources/views/partials/core/main-nav.blade.php
@@ -3,17 +3,32 @@
         <!-- Nav tabs -->
         <ul class="nav nav-tabs|pills" id="navId">
             <li class="nav-item">
-                <a href="#tab1Id" class="nav-link active">Home</a>
+                <a href="/" class="nav-link active">Home</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="#tab2Id">Action</a>
                     <a class="dropdown-item" href="#tab3Id">Another action</a>
                     <div class="dropdown-divider"></div>
                     <a class="dropdown-item" href="#tab4Id">Action</a>
                 </div>
-            </li>           
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#tab2Id">Action</a>
+                    <a class="dropdown-item" href="#tab3Id">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#tab4Id">Action</a>
+                </div>
+            </li>  
+            <li class="nav-item"><a href="/" class="nav-link"> About Us </a></li>
+            <li class="nav-item"><a href="/" class="nav-link">Become a Distributor</a></li>
+            <li class="nav-item"><a href="/" class="nav-link">Find a Stockist</a></li>
+            <li class="nav-item"><a href="/" class="nav-link">Spectrum Industrial UAE</a></li>
+            <li class="nav-item"><a href="/" class="nav-link">News</a></li>  
+            <li class="nav-item"><a href="/" class="nav-link">Catalogues</a></li>                        
         </ul>
            
         <!-- Tab panes -->
