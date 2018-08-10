33f1d0b657361229f9787dbe651d87a75e976bc2
Peter Day <peterday.main@gmail.com>
2018-08-10 11:26:32 +0100

    Unscrew YT Controller

--- app/Http/Controllers/Spectrum/YoutubeController.php
+++ app/Http/Controllers/Spectrum/YoutubeController.php
@@ -6,38 +6,38 @@ use App\Http\Controllers\Controller;
 
 class YoutubeController extends Controller{
 
-    //Get Youtube Video ID from database
-    $VideoID = DB::table('videos')->get();
+    public function GetVideos(){
+        //Get Youtube Video ID from database
+        $VideoID = DB::table('videos')->get();
 
-    // Set API Key
-    $ApiKey = 'AIzaSyDEwPpFGRrJdcoVxqTAWIEPKN5yDxhldbY';
+        // Set API Key
+        $ApiKey = 'AIzaSyDEwPpFGRrJdcoVxqTAWIEPKN5yDxhldbY';
 
-    $curl = curl_init();
+        $curl = curl_init();
 
-    curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://www.googleapis.com/youtube/v3/videos/?id=$VideoID&part=snippet%2CcontentDetails%2Cstatistics&key=$ApiKey",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 30,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "GET",
-    CURLOPT_HTTPHEADER => array(
-    "Cache-Control: no-cache",
-    "Postman-Token: 5a0f872b-175b-4eb3-965b-73fcfffb9747"
-    ),
-));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://www.googleapis.com/youtube/v3/videos/?id=$VideoID&part=snippet%2CcontentDetails%2Cstatistics&key=$ApiKey",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+            "Cache-Control: no-cache",
+            "Postman-Token: 5a0f872b-175b-4eb3-965b-73fcfffb9747"
+            ),
+        ));
 
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-    curl_close($curl);
+        curl_close($curl);
 
-    if ($err) {
-    echo "cURL Error #:" . $err;
-    } else {
-    echo $response;
+        return $response;
+        
     }
+
 }
 
 
