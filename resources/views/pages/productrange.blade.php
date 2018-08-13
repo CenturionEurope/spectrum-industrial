f4b86419ab2e629fbca1be7d99f1af3ac1b9f251
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-13 12:01:44 +0100

    category images & productrange updates

--- resources/views/pages/productrange.blade.php
+++ resources/views/pages/productrange.blade.php
@@ -8,23 +8,26 @@
     @include('partials.elements.main-slider')
     <div class="container">
         <div class="row content-area">   
-            <pre>{{print_r($Api)}}</pre>      
-            @foreach($Api->Product as $product)      
+            <pre>{{print_r($Api)}}</pre>                 
             <div class="container">
                 <div class="row">
+                @foreach($Api->Product as $product) 
                     <div class="col-lg-4">
                         <div class="categoryholder">
                             <div class="category-image">
-                                <img src="{{Voyager::image($product->CategoryImage)}}" alt="">
+                                <img src="{{Voyager::image($product->Category->CategoryImage)}}" alt="{{$product->Category->name}}">
                             </div>
                             <div class="category-name">
-                               <h4>{{$product->Category->name}}</h4>                            
+                                <a href="{{$product->Category->slug}}" title="{{$product->Category->name}}">
+                                    <h4 class="center">{{$product->Category->name}}</h4>
+                                </a>                          
                             </div>
                         </div>
                     </div>
+                @endforeach
                 </div>
             </div>
-            @endforeach
+            
         </div>   
     </div>
     @include('partials.core.footer')
