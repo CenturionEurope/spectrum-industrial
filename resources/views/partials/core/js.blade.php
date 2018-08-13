e3fa46b247565a8abf222f97ae1af277cac5c066
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-13 09:50:15 +0100

    moved schema

--- resources/views/partials/core/js.blade.php
+++ resources/views/partials/core/js.blade.php
@@ -8,20 +8,6 @@
 <script src="/js/product-tiles.js"></script>
 
 
-<!-- Product Schema -->
-<script type="text/javascript">
-console.log("this is a {{ $Api->Product->ProductName }} product page in {{$Api->Product->CategoryInfo->name or ''}} category");
-</script>
-<script type="application/ld+json">    
-    {
-      "@context": "http://schema.org/", 
-      "@type": "Product", 
-      "name": "{{ $Api->Product->ProductName }}",
-      "image": "{{Voyager::image($Api->Product->FeatureImage)}}",
-      "description": "qwdqqwdq",
-      "brand": "Spectrum",
-      "sku": "{{$Api->Product->ProductCode}}"
-    }
-</script>
+
 <!-- Cookie Consent -->
 @include('cookieConsent::index')
