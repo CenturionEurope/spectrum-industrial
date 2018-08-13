ef3e26e7f5b861d28d3363a94db5e0c818ae9d21
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-13 10:44:43 +0100

    video grid layout

---
+++ resources/views/partials/elements/video-grid.blade.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="latest-video">
+
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="older-video">
+
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="older-video">
+
+            </div>
+        </div>
+    </div>
+</div>
+
--- routes/web.php
+++ routes/web.php
@@ -12,65 +12,65 @@
 */
 
 // General Pages //
-Route::get('/', function () {
-    $Api = (object)array(
-        'Posts' => App\Post::all(),
-        // 'YouTube' => app('App\Http\Controllers\Spectrum\YoutubeController')->GetVideos() - Turned off
-    );    
-    return view('pages.welcome')->with('Api', $Api);
-});
+    Route::get('/', function () {
+        $Api = (object)array(
+            'Posts' => App\Post::all(),
+            // 'YouTube' => app('App\Http\Controllers\Spectrum\YoutubeController')->GetVideos() - Turned off
+        );    
+        return view('pages.welcome')->with('Api', $Api);
+    });
 
 // All Posts (News Page)
-Route::get('news', function(){
-    $Api = (object)array(
-        'Posts' => App\Post::all()
-    );    
-	return view('pages.news')->with('Api', $Api);
-});
+    Route::get('news', function(){
+        $Api = (object)array(
+            'Posts' => App\Post::all()
+        );    
+        return view('pages.news')->with('Api', $Api);
+    });
 
 // All Products (Primary Products Page)
-Route::get('products', function(){
-    $products = App\Product::all();        
-	return view('pages.productrange', compact('products'));
-});
+    Route::get('products', function(){
+        $products = App\Product::all();        
+        return view('pages.productrange', compact('products'));
+    });
 
 //Category Page
-Route::get('category/{Category}', function($Category){
-    $Api = (object)array(
-        'Product' => app('App\Http\Controllers\Spectrum\ProductController')->GetProduct($Category),
-    );
-	return view('pages.category')->with('Api', $Api);
-});
+    Route::get('category/{Category}', function($Category){
+        $Api = (object)array(
+            'Product' => app('App\Http\Controllers\Spectrum\ProductController')->GetProduct($Category),
+        );
+        return view('pages.category')->with('Api', $Api);
+    });
 
 
 // Templating Pages
 // Single Posts from the News Section 
-Route::get('post/{slug}', function($slug){
-    $post = App\Post::where('slug', '=', $slug)->firstOrFail();
-    $Api = (object)array(
-        'Posts' => App\Post::all()
-    );    
-	return view('pages.post', compact('post','Api'));
-});
+    Route::get('post/{slug}', function($slug){
+        $post = App\Post::where('slug', '=', $slug)->firstOrFail();
+        $Api = (object)array(
+            'Posts' => App\Post::all()
+        );    
+        return view('pages.post', compact('post','Api'));
+    });
 
 //Single Product
-Route::get('product/{slug}', function($slug){
+    Route::get('product/{slug}', function($slug){
 
-    $Api = (object)array(
-        'Product' => app('App\Http\Controllers\Spectrum\ProductController')->GetProduct($slug),
-        'Posts' => App\Post::all()        
-    );
+        $Api = (object)array(
+            'Product' => app('App\Http\Controllers\Spectrum\ProductController')->GetProduct($slug),
+            'Posts' => App\Post::all()        
+        );
 
-	return view('pages.product')->with('Api', $Api);
-});
+        return view('pages.product')->with('Api', $Api);
+    });
 
 // Custom Routes Below this Point 
 // LESS Controller 
-Route::get('/Resources/LESS', ['uses' =>'Spectrum\LessController@Less']);
+    Route::get('/Resources/LESS', ['uses' =>'Spectrum\LessController@Less']);
 
 // Voyager Admin 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+    Route::group(['prefix' => 'admin'], function () {
+        Voyager::routes();
+    });
 
 
