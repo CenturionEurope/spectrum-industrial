72cdaa6e027bffbf7452cecf5f5bd27f3322e389
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-13 09:38:24 +0100

    removal of spatie/laravel-feed

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -12,7 +12,7 @@
                 <pre>{{print_r($Api)}}</pre>
                <h4>Latest News</h4>
                <hr>
-               @foreach($Posts as $post)
+               @foreach($posts as $post)
                 <div class="news-article">
                     <div class="feature-image">
                         <a href="/post/{{ $post->slug }}"><img src="{{ Voyager::image( $post->image ) }}" alt="{{$post->title}}"></a>
