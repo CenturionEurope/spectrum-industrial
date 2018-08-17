93022991590d1bc6209c5179d09763e99230f2d2
Nathan Langer <nlanger@centurioneurope.co.uk>
2018-08-17 10:41:32 +0100

    test

--- app/Http/Controllers/Spectrum/Mailing/AbandonCartController.php
+++ app/Http/Controllers/Spectrum/Mailing/AbandonCartController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Spectrum;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
-class SearchController extends Controller{
+class AbandonCartController extends Controller{
 
     public function(){
         
